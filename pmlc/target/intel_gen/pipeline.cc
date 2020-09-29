3f0b2f234f08213e5007204c45b5bf46e0e5bc73
XingHongChenIntel <xinghong.chen@intel.com>
2020-09-29 17:06:15 +0800

    open gpu_to_vulkan pass.

--- pmlc/target/intel_gen/pipeline.cc
+++ pmlc/target/intel_gen/pipeline.cc
@@ -22,9 +22,11 @@
 #include "pmlc/conversion/gpu_to_spirv/passes.h"
 #include "pmlc/conversion/pxa_to_affine/passes.h"
 #include "pmlc/conversion/stdx_to_llvm/passes.h"
+#include "pmlc/conversion/gpu_to_vulkan/passes.h"
 #include "pmlc/conversion/tile_to_pxa/passes.h"
 #include "pmlc/dialect/pxa/transforms/passes.h"
 #include "pmlc/dialect/stdx/ir/ops.h"
+#include "pmlc/dialect/vulkan/ir/ops.h"
 #include "pmlc/dialect/stdx/transforms/passes.h"
 #include "pmlc/dialect/tile/transforms/passes.h"
 #include "pmlc/target/intel_gen/pass_detail.h"
@@ -181,7 +183,8 @@ void pipelineBuilder(OpPassManager &pm) {
   pm.addPass(spirv::createUpdateVersionCapabilityExtensionPass());
 
   // GPU to Vulkan.
-  pm.addPass(conversion::gpu::createConvertGpuLaunchFuncToVulkanCallsPass());
+  pm.addPass(conversion::gpu_to_vulkan::createConvertGpuToVulkanPass());
+//  pm.addPass(conversion::gpu::createConvertGpuLaunchFuncToVulkanCallsPass());
 
   // Convert Vulkan calls to LLVM code
   pm.addPass(createConvertStandardToLLVM());
