bf3c709c2d26375f62e728e122efcbe4e15e48a2
zhangding <ding.zhang@intel.com>
2020-12-08 08:34:16 -0500

    update readvalue with some comments

--- plaidml/bridge/openvino/ops/read_value.cpp
+++ plaidml/bridge/openvino/ops/read_value.cpp
@@ -26,8 +26,9 @@ void registerReadValue() {
     IE_ASSERT(ctx.operands.size() == 1);
     auto I = ctx.operands.at(0);
     I = I + 0;  // Plaidml doesn't support return without operation
-    auto variable = layer->get_variable();
-    auto variable_info = variable->get_info();
+    // it can't work with it replace I = I+0 operation
+    // auto C = I;
+    auto variable_info = layer->get_variable()->get_info();
     auto variable_type = variable_info.data_type;
     auto variable_shape = variable_info.data_shape;
     auto variable_id = variable_info.variable_id;
@@ -36,12 +37,15 @@ void registerReadValue() {
     if (variable_id != layer->get_variable_id()) {
       THROW_IE_EXCEPTION << "Variables identifiers are inconsistent.";
     }
+    // check type of output
     if (output_type != variable_type) {
       THROW_IE_EXCEPTION << "Output types are inconsistent.";
     }
+    // check whether shape is a dynamic
     if (variable_shape.is_dynamic()) {
       THROW_IE_EXCEPTION << "Dynamic shapes not currently supported by PlaidML plugin.";
     }
+    // check shape of the output
     auto shape = variable_shape.to_shape();
     if (shape != output_shape) {
       THROW_IE_EXCEPTION << "Output shapes are inconsistent.";
