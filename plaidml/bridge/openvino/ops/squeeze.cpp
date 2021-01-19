f81dac762ddc71568ea4d04b00638f79f41080ce
XingHongChenIntel <xinghong.chen@intel.com>
2021-01-19 16:51:08 +0800

    add get_constant_vector func into plaidml_util.hpp, fix squeeze op failed test case.

--- plaidml/bridge/openvino/ops/squeeze.cpp
+++ plaidml/bridge/openvino/ops/squeeze.cpp
@@ -19,7 +19,7 @@ void registerSqueeze() {
   registerOp("squeeze", [](const Context& ctx) {
     IE_ASSERT(ctx.operands.size() == 2);
     auto I = ctx.operands.at(0);
-    auto axes = get_axis_set_from_constant_operand(1, ctx.layer);
+    auto axes = get_constant_vector<int>(1, ctx.layer);
     std::vector<int> v_axes;
     v_axes.assign(axes.begin(), axes.end());
     return edsl::make_tuple(op::squeeze(I, v_axes));
