1c55ddaae193d019c0f9d157a77f755de8269a6c
Ling, Liyang <liyang.ling@intel.com>
2021-06-16 11:27:46 +0800

    Update Cells op test cases

--- plaidml/bridge/openvino/tests/functional/shared_tests_instances/single_layer_tests/gru_cell.cpp
+++ plaidml/bridge/openvino/tests/functional/shared_tests_instances/single_layer_tests/gru_cell.cpp
@@ -11,8 +11,8 @@ using namespace LayerTestsDefinitions;
 
 namespace {
 std::vector<bool> should_decompose{
-    // true will decompose rnn cell to component ops and
-    // skip plaidml plugin implementation, only set to false here
+    // true will decompose GRUCell to component ops and skip plaidml GRUCell implementation
+    true,
     false,
 };
 std::vector<size_t> batch{5};
