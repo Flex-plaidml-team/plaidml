aed28a6555312abc06c9b5204292521fa5a735d0
liyangling <liyang.ling@intel.com>
2021-07-19 13:49:02 +0800

    update floor and pow

--- plaidml/bridge/openvino/tests/functional/shared_tests_instances/single_layer_tests/lrn.cpp
+++ plaidml/bridge/openvino/tests/functional/shared_tests_instances/single_layer_tests/lrn.cpp
@@ -16,7 +16,7 @@ namespace {
 
 const std::vector<InferenceEngine::Precision> netPrecisions = {
     InferenceEngine::Precision::FP32,
-    // InferenceEngine::Precision::FP16,
+    InferenceEngine::Precision::FP16,
 };
 
 const std::vector<std::vector<int64_t>> axes = {
